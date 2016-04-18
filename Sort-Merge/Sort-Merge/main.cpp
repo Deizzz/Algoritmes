106a101c4b40006ad09615620370bc65b78a8cc5
Deizzz <kozlovvladimir6@gmail.com>
2016-04-18 18:18:12 +0300

    Merge sort algoritm

--- Sort-Merge/Sort-Merge/main.cpp
+++ Sort-Merge/Sort-Merge/main.cpp
@@ -9,7 +9,31 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int Arr[] = "2,6,1,4,3,9,10,5,7";
+    int r, q,p,n1,n2;
+    n1 = q-p+1;
+    n2 = r-q;
+    int Arr1[n1], Arr2[n2];
+    int i,j;
+    for(i = 0; i< n1; i++){
+        Arr1[i] = Arr[p+i+1];
+    }
+    for(j = 0; j<n2;j++){
+        Arr2[j] = Arr[q+j];
+    }
+    i=0;j=0;
+    
+    for(int k = p;k<r;k++){
+        if(Arr2[i]<=Arr1[j]){
+            Arr[k] = Arr2[i];
+            i+=1;
+        }
+        else{
+            Arr[k] = Arr1[j];
+            j+=1;
+        }
+    }
+    
     return 0;
 }
